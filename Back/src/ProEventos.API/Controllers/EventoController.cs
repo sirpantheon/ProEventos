7f3cff936f52b904b8285b537d059455412cd9ba
johnatan sousa paixao <johnatan.sousa@gmail.com>
2022-07-12 21:03:10 -0300

    EventoController

---
+++ Back/src/ProEventos.API/Controllers/EventoController.cs
@@ -0,0 +1,19 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProEventos.API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class EventoController : ControllerBase
+{
+    public EventoController()
+    {
+    
+    }
+
+    [HttpGet]
+    public string Get()
+    {
+        return "value";
+    }
+}
